e04c6709164d2f7c46bf466d77f781a3420ef222
sajjadhira <sajjad.hira12@gmail.com>
2024-06-07 04:49:07 +0600

    Showing data to the index page and clicked to copy this data.

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,70 @@
-import React from 'react'
+'use client';
+
+import {useState, useEffect} from 'react'
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({data, handleTagClick}) => {
+  return (
+    <div className="mt-16 prompt_layout">
+     {
+      data.map((prompt, index) => {
+        return (
+          <PromptCard
+          key={prompt._id}
+          prompt={prompt}
+          handleTagClick={handleTagClick}
+          />
+        )
+      })
+     }
+    </div>
+  )
+}
 
 const Feed = () => {
+
+  const [searchText, setSearchText] = useState('');
+  const [Prompts, setPrompts] = useState([])
+  const handleSearchChange = (e) => {
+    e.preventDefault();
+    setSearchText(e.target.value);
+  }
+
+  useEffect(() => {
+    // Fetch data from API
+    const fetechPost = async () => {
+      try {
+        let response = await fetch('/api/prompt');
+        if(response.ok) {
+          let data = await response.json();
+          setPrompts(data);
+        }
+      }
+      catch(error) {
+        console.error(error);
+      }
+    }
+
+    fetechPost();
+  }, [])
+
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form className='relative w-full flex-center'>
+        <input type="text"
+        placeholder='Search for prompts'
+        value={searchText}
+        onChange={handleSearchChange}
+        required
+        className='search_input peer'
+        />
+      </form>
+
+      <PromptCardList
+      data={Prompts}
+      handleTagClick={() => {}}
+       />
+    </section>
   )
 }
 
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,8 +1,73 @@
-import React from 'react'
+'use client';
 
-const PromptCard = () => {
+import { useState} from 'react'
+import Image from 'next/image';
+import { useSession } from 'next-auth/react';
+import { usePathname, useRouter } from 'next/navigation';
+
+const PromptCard = ({prompt, handleTagClick, handleEdit, handleDelete}) => {
+
+  const [Copied, setCopied] = useState("");  
+  const handleCopy = () => {
+    navigator.clipboard.writeText(prompt.prompt);
+    setCopied(prompt.prompt);
+    setTimeout(() => {
+      setCopied("");
+    }, 3000);
+  }
   return (
-    <div>PromptCard</div>
+    <div className='prompt_card'>
+      <div className="flex justify-between item-start gap-5">
+        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+
+{prompt?.creator ? (
+  <div className="flex items-center">
+    <Image
+      src={prompt.creator.image}
+      alt={prompt.creator.username}
+      width={40}
+      height={40}
+      className="rounded-full object-contain"
+    />
+    <div className="flex flex-col ml-2">
+      <h3 className="font-satoshi font-semibold text-gray-900">{prompt.creator.username}</h3>
+      <p className="font-inter text-sm text-gray-500">{prompt.creator.email}</p>
+    </div>
+  </div>
+) : (
+  <div className="flex items-center">
+    <Image
+      src="/assets/images/logo.svg"
+      alt="Promptopia"
+      width={40}
+      height={40}
+      className="rounded-full object-contain"
+    />
+    <div className="flex flex-col ml-2">
+      <h3 className="font-satoshi font-semibold text-gray-900">Promptopia</h3>
+      <p className="font-inter text-sm text-gray-500">@promptopia</p>
+    </div>
+  </div>
+)}
+
+
+
+        </div>
+
+        <div className="copy_btn" onClick={handleCopy}>
+          <Image
+            src={Copied === prompt.prompt ? "/assets/icons/tick.svg" : "/assets/icons/copy.svg"}
+            alt="Copy"
+            width={20}
+            height={20}
+          />
+        </div>
+      </div>
+      <p className='my-4 font-satoshi text-sm text-gray-700'>{prompt.prompt}</p>
+      <p className='font-inter text-sm blue_gradient cursor-pointer'
+      onClick={() => handleTagClick ? handleTagClick(prompt.tag) : null}
+      >{prompt.tag}</p>
+    </div>
   )
 }
 
