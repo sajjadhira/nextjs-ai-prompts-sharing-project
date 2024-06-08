78636be29116105f63a720c33086b6097cad2b74
sajjadhira <sajjad.hira12@gmail.com>
2024-06-08 13:45:25 +0600

    no cache added to the endpoint

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -2,19 +2,27 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
+const setNoCacheHeaders = (response) => {
+    response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    response.headers.set('Pragma', 'no-cache');
+    response.headers.set('Expires', '0');
+    return response;
+};
+
 export const GET = async (req) => {
     try {
         await connectToDB();
         const prompts = await Prompt.find().populate("creator");
-        return new Response(JSON.stringify(prompts), {status: 200,
-            headers: {
-                'Cache-Control': 'no-store'
-            }});
+        let response = new Response(JSON.stringify(prompts), { status: 200 });
+        response = setNoCacheHeaders(response);
+        return response;
     } catch (e) {
-        console.error(e);
-        return new Response("Error fetching prompts", {status: 500,
-            headers: {
-                'Cache-Control': 'no-store'
-            }});
+        let response = new Response(JSON.stringify(
+            {
+                message: "Error fetching prompts ".concat(e.message)
+            }
+        ), { status: 500 });
+        response = setNoCacheHeaders(response);
+        return response;
     }
-}
+};
